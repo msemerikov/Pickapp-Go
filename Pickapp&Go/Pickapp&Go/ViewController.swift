1afc78915b5bc6242dc8aee1b0cbe3e92840e099
msemerikov <msemerikov@hotmail.com>
2020-05-05 16:25:27 +0500

    First commit

---
+++ Pickapp&Go/Pickapp&Go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pickapp&Go
+//
+//  Created by Mikhail Semerikov on 05.05.2020.
+//  Copyright © 2020 Mikhail Semerikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
